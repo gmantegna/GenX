0fc66ac258d2eeb23d93e0861233806d660cfb35
Gabriel A. Mantegna <gm1710@della8.princeton.edu>
2023-06-07 18:12:52 -0400

    comment out problematic code for when using VRESTOR module without colocation

--- src/write_outputs/write_storagedual.jl
+++ src/write_outputs/write_storagedual.jl
@@ -54,21 +54,21 @@ function write_storagedual(path::AbstractString, inputs::Dict, setup::Dict, EP::
 		end
 	end
 
-	# Loop over W for VRE-Storage resources
-	if !isempty(VS_STOR)
-		VS_STOR = inputs["VS_STOR"]
-		VRE_STOR_ALL_NONLDS = setdiff(VRE_STOR, inputs["VS_LDS"])
-		VRE_STOR_ALL_LDS = intersect(VRE_STOR, inputs["VS_LDS"])
-		dual_values[VS_STOR, INTERIOR_SUBPERIODS] = (dual.(EP[:cSoCBalInterior_VRE_STOR][INTERIOR_SUBPERIODS, VS_STOR]).data ./ inputs["omega"][INTERIOR_SUBPERIODS])'
-		dual_values[VRE_STOR_ALL_NONLDS, START_SUBPERIODS] = (dual.(EP[:cSoCBalStart_VRE_STOR][START_SUBPERIODS, VRE_STOR_ALL_NONLDS]).data ./ inputs["omega"][START_SUBPERIODS])'
-		if !isempty(VRE_STOR_ALL_LDS)
-			if setup["OperationWrapping"] == 1
-				dual_values[VRE_STOR_ALL_LDS, START_SUBPERIODS] = (dual.(EP[:cVreStorSoCBalLongDurationStorageStart][1:REP_PERIOD, VRE_STOR_ALL_LDS]).data ./ inputs["omega"][START_SUBPERIODS])'
-			else
-				dual_values[VRE_STOR_ALL_LDS, START_SUBPERIODS] = (dual.(EP[:cSoCBalStart_VRE_STOR][START_SUBPERIODS, VRE_STOR_ALL_LDS]).data ./ inputs["omega"][START_SUBPERIODS])'
-			end
-		end
-	end
+	# # Loop over W for VRE-Storage resources
+	# if !isempty(VS_STOR)
+	# 	VS_STOR = inputs["VS_STOR"]
+	# 	VRE_STOR_ALL_NONLDS = setdiff(VRE_STOR, inputs["VS_LDS"])
+	# 	VRE_STOR_ALL_LDS = intersect(VRE_STOR, inputs["VS_LDS"])
+	# 	dual_values[VS_STOR, INTERIOR_SUBPERIODS] = (dual.(EP[:cSoCBalInterior_VRE_STOR][INTERIOR_SUBPERIODS, VS_STOR]).data ./ inputs["omega"][INTERIOR_SUBPERIODS])'
+	# 	dual_values[VRE_STOR_ALL_NONLDS, START_SUBPERIODS] = (dual.(EP[:cSoCBalStart_VRE_STOR][START_SUBPERIODS, VRE_STOR_ALL_NONLDS]).data ./ inputs["omega"][START_SUBPERIODS])'
+	# 	if !isempty(VRE_STOR_ALL_LDS)
+	# 		if setup["OperationWrapping"] == 1
+	# 			dual_values[VRE_STOR_ALL_LDS, START_SUBPERIODS] = (dual.(EP[:cVreStorSoCBalLongDurationStorageStart][1:REP_PERIOD, VRE_STOR_ALL_LDS]).data ./ inputs["omega"][START_SUBPERIODS])'
+	# 		else
+	# 			dual_values[VRE_STOR_ALL_LDS, START_SUBPERIODS] = (dual.(EP[:cSoCBalStart_VRE_STOR][START_SUBPERIODS, VRE_STOR_ALL_LDS]).data ./ inputs["omega"][START_SUBPERIODS])'
+	# 		end
+	# 	end
+	# end
 
 	if setup["ParameterScale"] == 1
 	    dual_values *= ModelScalingFactor
