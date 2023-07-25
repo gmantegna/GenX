d32eccca8bd6bba9bf7b5547b35e43b52e9880d4
gmantegna <30732358+gmantegna@users.noreply.github.com>
2023-07-25 14:51:25 -0700

    Add VREStor module and associated documentation

--- src/write_outputs/capacity_reserve_margin/write_virtual_discharge.jl
+++ src/write_outputs/capacity_reserve_margin/write_virtual_discharge.jl
@@ -1,7 +1,7 @@
 @doc raw"""
 	write_virtual_discharge(path::AbstractString, inputs::Dict, setup::Dict, EP::Model)
-
-Function for writing the "virtual" discharge of each storage technology. Virtual discharge is used to
+	
+	Function for writing the "virtual" discharge of each storage technology. Virtual discharge is used to
 	allow storage resources to contribute to the capacity reserve margin without actually discharging.
 """
 function write_virtual_discharge(path::AbstractString, inputs::Dict, setup::Dict, EP::Model)
@@ -9,6 +9,7 @@ function write_virtual_discharge(path::AbstractString, inputs::Dict, setup::Dict
 	G = inputs["G"]     # Number of resources (generators, storage, DR, and DERs)
 	T = inputs["T"]     # Number of time steps (hours)
 	STOR_ALL = inputs["STOR_ALL"]
+	VRE_STOR = inputs["VRE_STOR"]
 
 	dfVirtualDischarge = DataFrame(Resource = inputs["RESOURCES"], Zone = dfGen[!,:Zone], AnnualSum = Array{Union{Missing,Float64}}(undef, G))
 	virtual_discharge = zeros(G,T)
@@ -16,6 +17,17 @@ function write_virtual_discharge(path::AbstractString, inputs::Dict, setup::Dict
 	scale_factor = setup["ParameterScale"] == 1 ? ModelScalingFactor : 1
 	if !isempty(STOR_ALL)
 	    virtual_discharge[STOR_ALL, :] = (value.(EP[:vCAPRES_discharge][STOR_ALL, :]).data - value.(EP[:vCAPRES_charge][STOR_ALL, :]).data) * scale_factor
+	end
+    if !isempty(VRE_STOR)
+		DC_DISCHARGE = inputs["VS_STOR_DC_DISCHARGE"]
+		DC_CHARGE = inputs["VS_STOR_DC_CHARGE"]
+		AC_DISCHARGE = inputs["VS_STOR_AC_DISCHARGE"]
+		AC_CHARGE = inputs["VS_STOR_AC_CHARGE"]
+		dfVRE_STOR = inputs["dfVRE_STOR"]
+	    virtual_discharge[DC_DISCHARGE, :] .+= (value.(EP[:vCAPRES_DC_DISCHARGE][DC_DISCHARGE, :]).data .* dfVRE_STOR[(dfVRE_STOR.STOR_DC_DISCHARGE.!=0), :EtaInverter]) * scale_factor
+		virtual_discharge[DC_CHARGE, :] .-= (value.(EP[:vCAPRES_DC_CHARGE][DC_CHARGE, :]).data ./ dfVRE_STOR[(dfVRE_STOR.STOR_DC_CHARGE.!=0), :EtaInverter]) * scale_factor
+		virtual_discharge[AC_DISCHARGE, :] .+= (value.(EP[:vCAPRES_AC_DISCHARGE][AC_DISCHARGE, :]).data) * scale_factor
+		virtual_discharge[AC_CHARGE, :] .-= (value.(EP[:vCAPRES_AC_CHARGE][AC_CHARGE, :]).data) * scale_factor
 	end
 
 	dfVirtualDischarge.AnnualSum .= virtual_discharge * inputs["omega"]
