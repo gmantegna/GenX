3eaa6f4d1f7bc141d692f97073f54390b3e41ccd
manarjar <manarjar01@gmail.com>
2024-09-25 13:44:57 -0400

    Add all files related to load, model, print and test PRM policy
        if setup["PlanningReserveMargin"] == 1
            elapsed_time_prm_penalties= @elapsed write_prm_prices(path, inputs, setup, EP)
            println("Time elapsed for writing prm penalties is")
            println(elapsed_time_prm_penalties)
        end
        if setup["PlanningReserveMargin"] == 1
            elapsed_time_prm_penalties= @elapsed write_prm_prices(path, inputs, setup, EP)
            println("Time elapsed for writing prm penalties is")
            println(elapsed_time_prm_penalties)
        end
        if setup["PlanningReserveMargin"] == 1
            elapsed_time_prm_penalties= @elapsed write_prm_prices(path, inputs, setup, EP)
            println("Time elapsed for writing prm penalties is")
            println(elapsed_time_prm_penalties)
        end
        if setup["PlanningReserveMargin"] == 1
            elapsed_time_prm_penalties= @elapsed write_prm_prices(path, inputs, setup, EP)
            println("Time elapsed for writing prm penalties is")
            println(elapsed_time_prm_penalties)
        end
        if setup["PlanningReserveMargin"] == 1
            elapsed_time_prm_penalties= @elapsed write_prm_prices(path, inputs, setup, EP)
            println("Time elapsed for writing prm penalties is")
            println(elapsed_time_prm_penalties)
        end
        if setup["PlanningReserveMargin"] == 1
            elapsed_time_prm_penalties= @elapsed write_prm_prices(path, inputs, setup, EP)
            println("Time elapsed for writing prm penalties is")
            println(elapsed_time_prm_penalties)
        end
        if setup["PlanningReserveMargin"] == 1
            elapsed_time_prm_p
    	enalties= @elapsed write_prm_prices(path, inputs, setup, EP)
            println("Time elapsed for writing prm penalties is")
            println(elapsed_time_prm_penalties)
        end

--- src/write_outputs/write_markets.jl
+++ src/write_outputs/write_markets.jl
@@ -6,20 +6,50 @@ Function for reporting non-served energy for every model zone, time step and cos
 
 
 function write_market(path::AbstractString, inputs::Dict, setup::Dict, EP::Model)
-    MZ = inputs["MZ"]     # Number of zones
+    Z = inputs["Z"]
+    MZ = inputs["MZ"] 
+    T = inputs["T"] 
     scale_factor = setup["ParameterScale"] == 1 ? ModelScalingFactor : 1
-
-    # Non-served energy/demand curtailment by segment in each time step
-    MSell =  transpose(value.(EP[:vMKT_SELL]).data)
-    MSell = MSell .*scale_factor
-    dfMarketSell = DataFrame(MSell, ["Market_Sell_$z" for z in MZ])
-
-
-    MBuy =  transpose(value.(EP[:vMKT_BUY]).data)
-    MBuy = MBuy .*scale_factor
-    dfMarketBuy = DataFrame(MBuy, ["Market_Buy_$z" for z in MZ])
-
-    CSV.write(joinpath(path, "market.csv"), hcat(dfMarketBuy, dfMarketSell))
+    
+    col_names = String[]
+    zones = Int[]
+    for i in 1:Z
+        push!(col_names, "Market_Buy_$i")
+        push!(zones, i)
+        push!(col_names, "Market_Sell_$i")
+        push!(zones, i)
+    end
+    
+    dfmarkets = DataFrame(Market = col_names,
+    Zone = zones,
+    AnnualSum = Array{Union{Missing, Float64}}(undef, length(col_names)))
+    markets = Matrix{Float64}(undef, length(col_names), T)
+        
+    # Fill the matrix with zeros
+    fill!(markets, 0.0)
+    
+    for i in 1:Z
+        if i in MZ
+            markets[2*i-1, :] = value.(EP[:vMKT_BUY][i,:]).data
+            markets[2*i, :] = value.(EP[:vMKT_SELL][i,:]).data
+        else
+            markets[2*i-1, :] = zeros(T)
+            markets[2*i, :] = zeros(T)
+        end
+    end
+    markets .*=scale_factor
+    dfmarkets.AnnualSum .= markets * inputs["omega"]
+    
+    filepath = joinpath(path, "market.csv")
+    if setup["WriteOutputs"] == "annual"
+        GenX.write_annual(filepath, dfmarkets)
+    else # setup["WriteOutputs"] == "full"
+        dfmarkets = GenX.write_fulltimeseries(filepath, markets, dfmarkets)
+        if setup["OutputFullTimeSeries"] == 1 && setup["TimeDomainReduction"] == 1
+            GenX.write_full_time_series_reconstruction(path, setup, dfmarkets, "market")
+            @info("Writing Full Time Series for markets")
+        end
+    end
 
     return nothing
 end
