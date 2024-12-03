b62f723268e2dcdcd1e370dfe00164b4d0a0c87e
Jim Himelic <jhimelic@firstprinciples.run>
2024-12-03 08:16:40 -0800

    jim's examples and debugging tool

---
+++ Jim_Examples/5_Ava_updated_3/call_genx.jl
@@ -0,0 +1,11 @@
+# This simple script is to call run.jl to run GenX
+
+# define file path 
+root_directory = pwd()
+folder_directory = "example_systems\\"
+model_name = "5_Ava_sales_off\\"
+file_name = "Run.jl"
+file_path = joinpath(root_directory, folder_directory, model_name, file_name)
+
+#call GenX
+include(file_path)
