eee21bdb5d3934ff01daa345a0a85d93feb28db7
manarjar <manarjar01@gmail.com>
2024-12-09 09:19:55 -0500

    modify write prm and esr files

--- src/write_outputs/write_prm_prices.jl
+++ src/write_outputs/write_prm_prices.jl
@@ -12,3 +12,27 @@ function write_prm_prices(path::AbstractString, inputs::Dict, setup::Dict, EP::M
     dfPRM .*= scale_factor
     CSV.write(joinpath(path, "PRM_prices_and_penalties.csv"), dfPRM)
 end
+
+function write_prm_resource_capacity(path::AbstractString, inputs::Dict, setup::Dict, EP::Model)
+    gen = inputs["RESOURCES"]
+    G = inputs["G"]
+
+    resources = [gen[y].resource for y in 1:G]
+    resource_zone = [gen[y].zone for y in 1:G]
+    resource_elcc = [elcc(gen[y], tag = 1) for y in 1:G]
+
+    EndCap = value.(EP[:eTotalCap])
+    PRMCap = resource_elcc .* EndCap
+    Percentage_PRM_Req = [PRMCap[i] /inputs["PRM"][gen[i].zone] for i in 1:G] .* 100
+
+
+    df_resource_prm = DataFrame(
+        Resource = resources,
+        Zone = resource_zone,
+        ELCC = resource_elcc,
+        End_Capacity = EndCap,
+        PRM_Capacity = PRMCap,
+        Per_Zonal_PRM_Req = Percentage_PRM_Req
+    )
+    CSV.write(joinpath(path, "PRM_resource_capacity.csv"), df_resource_prm)
+end
