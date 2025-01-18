2ce099bab2fd48dabd4cfb1af8dabd1f2af8c32a
manarjar <manarjar01@gmail.com>
2025-01-17 21:15:24 -0500

    fix esr output

--- src/write_outputs/energy_share_requirement/write_esr_prices.jl
+++ src/write_outputs/energy_share_requirement/write_esr_prices.jl
@@ -18,6 +18,11 @@ end
 
 function write_esr_detailed(path::AbstractString, inputs::Dict, setup::Dict, EP::Model)
     gen = inputs["RESOURCES"]
+    SOLAR = inputs["VS_SOLAR"]                                      # Set of VRE-STOR generators with solar-component
+    WIND = inputs["VS_WIND"]                                        # Set of VRE-STOR generators with wind-component
+    gen_VRE_STOR = gen.VreStorage                                   # Set of VRE-STOR generators (objects)
+    by_rid(rid, sym) = GenX.by_rid_res(rid, sym, gen_VRE_STOR)
+    
     scale_factor = setup["ParameterScale"] == 1 ? GenX.ModelScalingFactor : 1
        
     dfESR = DataFrame(ESR = ["ESR_$i" for i in 1:inputs["nESR"]])
@@ -28,8 +33,27 @@ function write_esr_detailed(path::AbstractString, inputs::Dict, setup::Dict, EP:
     
     dfESR[!,:ESR_met] = zeros(inputs["nESR"])
     for i in 1:inputs["nESR"]
-        dfESR[!, :ESR_met][i] = sum(inputs["omega"][t] * GenX.esr(gen[y], tag = i) * value(EP[:vP][y, t])
-        for y in GenX.ids_with_policy(gen, GenX.esr, tag = i), t in 1:inputs["T"]) * scale_factor
+        tmp_ids1 = GenX.ids_with_policy(gen, GenX.esr, tag = i)
+        tmp_ids2 = intersect(SOLAR, GenX.ids_with_policy(gen, GenX.esr_vrestor, tag = i))
+        tmp_ids3 = intersect(WIND, GenX.ids_with_policy(gen, GenX.esr_vrestor, tag = i))
+        dfESR[!, :ESR_met][i] = 0
+       
+        # First sum
+        dfESR[!, :ESR_met][i] += isempty(tmp_ids1) ? 0 :
+            sum(inputs["omega"][t] * GenX.esr(gen[y], tag = i) * value(EP[:vP][y, t])
+                for y in tmp_ids1, t in 1:inputs["T"])
+        
+        # Second sum
+        dfESR[!, :ESR_met][i] += isempty(tmp_ids2) ? 0 :
+            sum(inputs["omega"][t] * GenX.esr_vrestor(gen[y], tag = i) * value(EP[:vP_SOLAR][y, t])
+                for y in tmp_ids2, t in 1:inputs["T"])
+        
+        # Third sum
+        dfESR[!, :ESR_met][i] += isempty(tmp_ids3) ? 0 :
+            sum(inputs["omega"][t] * GenX.esr_vrestor(gen[y], tag = i) * value(EP[:vP_WIND][y, t])
+                for y in tmp_ids3, t in 1:inputs["T"])    
+        
+        dfESR[!, :ESR_met][i] *= scale_factor
     end
     
     if setup["IncludeLossesInESR"] == 1
--- src/write_outputs/energy_share_requirement/write_esr_shares.jl
+++ src/write_outputs/energy_share_requirement/write_esr_shares.jl
@@ -7,19 +7,42 @@ function write_esr_resource_shares(path::AbstractString,
         inputs::Dict,
         setup::Dict,
         EP::Model)
-    gen = inputs["RESOURCES"]
-    G = inputs["G"]
+        gen = inputs["RESOURCES"]
+        SOLAR = inputs["VS_SOLAR"]                                      # Set of VRE-STOR generators with solar-component
+        WIND = inputs["VS_WIND"]                                        # Set of VRE-STOR generators with wind-component
+        gen_VRE_STOR = gen.VreStorage                                   # Set of VRE-STOR generators (objects)
+        by_rid(rid, sym) = GenX.by_rid_res(rid, sym, gen_VRE_STOR)
+        
+        G = inputs["G"]
     
     total_demand = Dict(i => sum(inputs["pD"][:,i]) for i in 1:inputs["Z"])
     df = DataFrame(
-        resource = [gen[y].resource for y in 1:G],
-        zone = [gen[y].zone for y in 1:G],
-        energy = [sum(value.(EP[:vP][g,:])) for g in 1:G]    )
+    resource = [gen[y].resource for y in 1:G],
+    zone = [gen[y].zone for y in 1:G],
+    energy = [sum(value.(EP[:vP][g,:]) .* inputs["omega"]) for g in 1:G],
+    energy_vrestor = zeros(G) )
+
+    for y in SOLAR 
+        df[!, "energy_vrestor"][y] += sum(inputs["omega"] .* value.(EP[:vP_SOLAR][y, :])) *
+        by_rid(y, :etainverter) 
+    end
     
+    for y in WIND
+        df[!, "energy_vrestor"][y] += sum(inputs["omega"] .* value.(EP[:vP_WIND][y, :]))
+    end
+
+ 
     for i in 1:inputs["nESR"]
-        df[!,"participate_in_esr$i"] = [GenX.esr(gen[y], tag = i) for y in 1:G]
-        df[!,"percentage_of_esr_$i"] .= df[!,"energy"] .* df[!,"participate_in_esr$i"] ./ 
-            sum(inputs["dfESR"][z,i] * total_demand[z] for z in 1:inputs["Z"]) * 100  
+        df[!,"participate_in_esr_$i"] = [GenX.esr(gen[y], tag = i) for y in 1:G]
+        df[!,"participate_in_esr_vrestor_$i"] = [GenX.esr_vrestor(gen[y], tag = i) for y in 1:G]
+        tmp_demand = sum(inputs["dfESR"][z,i] * total_demand[z] for z in 1:inputs["Z"])
+
+        df[!,"percentage_of_esr_$i"] .= (df[!,"energy"] .* df[!,"participate_in_esr_$i"] ./ 
+            tmp_demand * 100) +
+            (df[!,"energy_vrestor"] .* df[!,"participate_in_esr_vrestor_$i"] ./ 
+            tmp_demand * 100) + 
+            (df[!,"energy_vrestor"] .* df[!,"participate_in_esr_vrestor_$i"] ./ 
+            tmp_demand * 100)
     end
 
     CSV.write(joinpath(path, "ESR_resource_shares.csv"), df)
